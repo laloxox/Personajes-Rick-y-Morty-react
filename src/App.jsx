0302387e7cf58caca829dad5df309375c129186f
lalo <lalopx555@gmail.com>
2023-01-17 12:54:28 -0600

    Cambios y actualizaciones menores

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CharacterList from './components/CharacterList';
+import Favicon from "react-favicon";
 
 
 
@@ -9,10 +10,11 @@ function App() {
   return  <div className='text-white'>
           <h1 className='text-center text-info fw-bold display-1 py-4 '>Rick y Morty</h1>
           <h4 className='text-center text-secondary fw-bold display-4 py-0 '>characters</h4>
-
+      
             <CharacterList/>
 
           </div>;
+          
     
 }
 
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,17 +2,16 @@ import {Fade} from 'react-awesome-reveal'
 
 function CharacterCard({ character }) {
     return (
+        
     <Fade>
-        <div className="text-center p-5">
-            
-            
+
+        <div className="text-center p-5">    
             <h2>{character.name}</h2>
             <img className="img-fluid rounded-pill" src={character.image} alt={character.name}/>
             <h4 className="text-secondary p-3">Origen: {character.origin.name}</h4>
-            <h5 className="text-primary p-1">{character.status}</h5>
-            
-            
+            <h5 className="text-primary p-1">{character.status}</h5>    
         </div>
+
     </Fade>
     );
 }
